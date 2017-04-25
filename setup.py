93b0b1584660f4e9d18c978c9f976246c071f87f
David McClure <davidwilliammcclure@gmail.com>
2017-04-25 10:35:51 -0700

    Add setup.py, to get imports working in Jupyter.

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='strip-gutenberg-paratext',
+    version='0.1.0',
+    description='Remove front and backmatter from Gutenberg text files.',
+    url='https://github.com/davidmcclure/strip-gutenberg-paratext',
+    license='MIT',
+    author='David McClure',
+    author_email='dclure@mit.edu',
+    packages=find_packages(),
+)
